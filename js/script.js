27c04ccd5a795d49bb9544378e5eea3fad8b184b
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-04-27 21:09:56 -0300

    Validacao e formulario

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function clicar() {
     window.alert('OLa')
 }
+let botao = document.getElementById('botao')
+
+document.getElementById('botao').addEventListener('mousemove', function() {
+    botao.style.backgroundColor = '#0d1821f4'
+});
+document.getElementById('botao').addEventListener('mouseleave', function(){
+    botao.style.backgroundColor = '#0d1821'
+});
